15046c8c3a5cff1ff2c8b4ef86f11e15a392b2b6
Gavyn <gavyn.riebau@gmail.com>
2017-05-11 19:25:57 +0800

    Added logging and argument for keysize

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,18 @@
 extern crate hamming;
 extern crate clap;
 
+#[macro_use] extern crate log;
+extern crate env_logger;
+
 use hamming::distance;
 use clap::{App, Arg};
 use std::fs::File;
 use std::io::{Read};
 
 fn main() {
+    env_logger::init().unwrap();
 
-    let mut matches = App::new("xor-keysize-guess")
+    let matches = App::new("xor-keysize-guess")
         .version("1.0.0")
         .author("Gavyn Riebau")
         .about("Guesses the most likely keysize used to XOR encrypt a given file.")
@@ -19,6 +23,12 @@ fn main() {
              .takes_value(true)
              .help("The input file for which the XOR encryption keysize will be guessed")
              .required(true))
+        .arg(Arg::with_name("MAX_KEYSIZE")
+             .short("k")
+             .long("keysize")
+             .takes_value(true)
+             .help("The maximum key size to guess")
+             .default_value("40"))
         .get_matches();
 
     let file_path = matches.value_of("INPUT").unwrap();
@@ -27,16 +37,22 @@ fn main() {
 
     let _ = file.read_to_end(&mut file_contents);
 
-    //println!("Input file path: {}", file_path);
+    debug!("Input file path: {}", file_path);
 
-    for keysize in (1..40) {
-        //println!("Keysize '{}'", keysize);
+    let max_keysize_str = matches.value_of("MAX_KEYSIZE").unwrap();
+    let mut max_keysize = max_keysize_str.parse::<usize>().unwrap();
+    max_keysize += 1;
+
+    for keysize in 1..max_keysize {
 
         let mut chunks = file_contents.chunks(keysize);
-        let mut chunk_count = 0;
+        let mut num_chunks_compared = 0;
         let mut average_hamming_dist = 0.0_f32;
 
-        for idx in (1..3) {
+        // Calculate the mean normalized hamming distance over a
+        // number of samples to try to improve accuracy.
+        for idx in 1..3 {
+
             let left_chunk = chunks.next();
             let right_chunk = chunks.next();
 
@@ -58,43 +74,17 @@ fn main() {
             let normalized_hamming = hamming_dist as f32 / keysize as f32;
             average_hamming_dist += normalized_hamming;
 
-            //println!("{:4.3} is the normalized hamming distance for keysize {} and block {}", normalized_hamming, keysize, idx);
-            chunk_count += 1;
+            debug!("{:4.3} is the normalized hamming distance for keysize {} and block {}", normalized_hamming, keysize, idx);
+
+            num_chunks_compared += 1;
         }
-        average_hamming_dist = average_hamming_dist / chunk_count as f32;
-        println!("{:4.3} average hamming distance for keysize {}", average_hamming_dist, keysize);
 
-
-        /*
-        loop {
-            let left_chunk = chunks.next();
-            let right_chunk = chunks.next();
-
-            if left_chunk.is_none() {
-                break;
-            }
-            if right_chunk.is_none() {
-                break;
-            }
-
-            let left = left_chunk.unwrap();
-            let right = right_chunk.unwrap();
-
-            if left.len() != right.len() {
-                break;
-            }
-
-            let hamming_dist = distance(left, right);
-            let normalized_hamming = hamming_dist as f32 / keysize as f32;
-            average_hamming_dist += normalized_hamming;
-
-            chunk_count += 1;
+        if num_chunks_compared != 0 {
+            average_hamming_dist = average_hamming_dist / num_chunks_compared as f32;
+            println!("{:4.3} average hamming distance for keysize {}", average_hamming_dist, keysize);
+        } else {
+            debug!("Not enough data in input file to check a keysize of '{}'", keysize);
         }
-        average_hamming_dist = average_hamming_dist / (chunk_count as f32);
-        println!("{:4.3} average hamming distance for keysize {}", average_hamming_dist, keysize);
-        */
+
     }
-
-
-
 }
